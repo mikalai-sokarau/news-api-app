44d153c07a5f9a568820114c38977ea85b2571a8
Mikalai Sokarau <mikalaisokarau@gmail.com>
2018-06-19 10:14:33 +0300

    fix typo

--- src/components/Error/index.js
+++ src/components/Error/index.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export const Error = () => (
   <div>
-    <h1>There are nothing interesting right now, try later!</h1>
+    <h1>There is nothing interesting right now, try later!</h1>
   </div>
 );
