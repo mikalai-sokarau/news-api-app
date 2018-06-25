e295fe664560da359ded01388a956a3a4df72de7
Mikalai Sokarau <mikalaisokarau@gmail.com>
2018-06-25 16:51:10 +0300

    wraped logo text in Link

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -6,17 +6,15 @@ const NavBar = () => (
     <nav>
       <div className="nav-wrapper indigo lighten-4 row">
         <div className="col s9">
-          <a href="/" className="bran-logo-center">
+          <Link to="/" className="bran-logo-center">
             MY FEED
-          </a>
+          </Link>
         </div>
         <div className="col s3">
-          <Link to="/favorite">
-            <label>
-              <input type="checkbox" className="filled-in" />
-              <span>Show favorite news</span>
-            </label>
-          </Link>
+          <label>
+            <input type="checkbox" className="filled-in" />
+            <span>Show favorite news</span>
+          </label>
         </div>
       </div>
     </nav>
