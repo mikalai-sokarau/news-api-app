672e2e2b24755400c7c810d8c05ed934dfcb1ba0
Mikalai Sokarau <mikalaisokarau@gmail.com>
2018-06-28 16:52:20 +0300

    News refactoring

--- src/containers/News/index.js
+++ src/containers/News/index.js
@@ -1,17 +1,18 @@
-import React, { PureComponent } from "react";
-import SingleNews from "../../components/SingleNews";
-import { NEWS_SOURCES } from "../../common/constants";
-import Error from "../Error";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import SingleNews from '../../components/SingleNews';
+import { NEWS_SOURCES } from '../../common/constants';
+import * as newsActions from '../../reducers/index';
+import Error from '../Error';
 
 const DEFAULT_NEWS = NEWS_SOURCES[0];
 
-class News extends PureComponent {
+class News extends Component {
   getNews = () => {
     const options = {
       consumer: 'news',
       source:
-        NEWS_SOURCES.find(item => item.shortName === this.props.source) ||
-        DEFAULT_NEWS
+        NEWS_SOURCES.find(item => item.shortName === this.props.source) || DEFAULT_NEWS
     };
 
     this.props.getNewsFrom(options);
@@ -27,24 +28,34 @@ class News extends PureComponent {
     }
   }
 
-  renderNews = () =>
-    this.props.news.map(item => (
-      <SingleNews
-        key={item.url}
-        item={item}
-        checked={this.props.favoriteNewsKeys[item.url]}
-        addNewsToFavorite={this.props.addNewsToFavorite}
-        removeNewsFromFavorite={this.props.removeNewsFromFavorite}
-      />
-    ));
-
   render = () => (
     <div className="col s8">
       <Error>
-        <div>{this.renderNews()}</div>
+        <div>
+          {this.props.news.map(item => (
+            <SingleNews
+              key={item.url}
+              item={item}
+              checked={this.props.favoriteNewsKeys[item.url]}
+              addNewsToFavorite={this.props.addNewsToFavorite}
+              removeNewsFromFavorite={this.props.removeNewsFromFavorite}
+            />
+          ))}
+        </div>
       </Error>
     </div>
   );
 }
 
-export default News;
+const mapStateToProps = store => ({ ...store });
+
+const mapDispatchToProps = dispatch => ({
+  getNewsFrom: options => dispatch(newsActions.getNewsFrom(options)),
+  addNewsToFavorite: options => dispatch(newsActions.addNewsToFavorite(options)),
+  removeNewsFromFavorite: options => dispatch(newsActions.removeNewsFromFavorite(options))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(News);
