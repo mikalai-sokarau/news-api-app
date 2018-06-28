84cdfd7cff6c9e8532b5022055a27347b57ea0f4
Mikalai Sokarau <mikalaisokarau@gmail.com>
2018-06-28 11:39:33 +0300

    splited NavBar logic

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,37 +1,24 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { StyledDiv, StyledContainer } from "./style";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { StyledDiv, StyledContainer } from './style';
 
-const NavBar = ({ history, location }) => {
-  const buttonText =
-    location.pathname === "/favorite" ? "BACK" : "Favorite news";
-  const clickHandler = () => {
-    return location.pathname === "/favorite"
-      ? () => history.goBack()
-      : () => history.push("/favorite");
-  };
-
-  return (
-    <div className="navbar-fixed">
-      <nav>
-        <StyledContainer className="indigo lighten-4 row">
-          <div className="col s10">
-            <Link to="/" className="bran-logo-center">
-              MY FEED
-            </Link>
-          </div>
-          <StyledDiv className="col s2">
-            <a
-              onClick={clickHandler()}
-              className="waves-effect waves-light btn-small"
-            >
-              {buttonText}
-            </a>
-          </StyledDiv>
-        </StyledContainer>
-      </nav>
-    </div>
-  );
-};
+const NavBar = ({ clickHandler, location: { pathname } }) => (
+  <div className="navbar-fixed">
+    <nav>
+      <StyledContainer className="indigo lighten-4 row">
+        <div className="col s10">
+          <Link to="/" className="bran-logo-center">
+            MY FEED
+          </Link>
+        </div>
+        <StyledDiv className="col s2">
+          <a onClick={clickHandler} className="waves-effect waves-light btn-small">
+            {pathname === '/favorite' ? 'back' : 'favorite news'}
+          </a>
+        </StyledDiv>
+      </StyledContainer>
+    </nav>
+  </div>
+);
 
 export default NavBar;
