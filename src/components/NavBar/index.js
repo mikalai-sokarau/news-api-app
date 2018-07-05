ff5aa9aa689c1418c026dcb5f37de3d8e91e1c0f
Mikalai Sokarau <mikalaisokarau@gmail.com>
2018-07-05 11:43:45 +0300

    wrapped navbar navigation in ul

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { StyledDiv, StyledContainer } from './style';
-import { StyledGlobe } from './style';
 import Button from '../Button';
 
 const NavBar = ({ clickHandler, buttonName }) => (
@@ -9,10 +8,19 @@ const NavBar = ({ clickHandler, buttonName }) => (
     <nav>
       <StyledContainer className="indigo lighten-4 row">
         <div className="col s10">
-          <Link to="/" className="bran-logof-center">
-            MY FEED
-          </Link>
-          <Link to="/map"><StyledGlobe></StyledGlobe></Link>
+          <ul>
+            <li>
+              <Link to="/" className="bran-logof-center">
+                MY FEED
+              </Link>|
+            </li>
+            <li>
+              <Link to="/images"> IMAGES </Link>|
+            </li>
+            <li>
+              <Link to="/map">MAP</Link>
+            </li>
+          </ul>
         </div>
         <StyledDiv className="col s2">
           <Button clickHandler={clickHandler} buttonName={buttonName} />
--- src/components/NavBar/style.js
+++ src/components/NavBar/style.js
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import globe from 'react-icons/lib/fa/globe'
 
 const StyledDiv = styled.div`
   display: flex;
@@ -11,17 +10,4 @@ const StyledContainer = styled.nav`
   display: flex;
 `;
 
-const StyledGlobe = styled(globe)`
-  font-size: 2rem;
-  margin-left: 15px;
-  transition-duration: 0.3s;
-  transition-timing-function: ease-out;
-  transition-delay: 0.15s;
-
-  :hover {
-    transform: rotate(360deg);
-    cursor: pointer;
-  }
-`;
-
-export { StyledDiv, StyledContainer, StyledGlobe };
+export { StyledDiv, StyledContainer };
