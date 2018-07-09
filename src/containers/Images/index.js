33b2afda8524b73a51c5662c926741323b2dab24
Mikalai Sokarau <mikalaisokarau@gmail.com>
2018-07-09 15:14:23 +0300

    Images refactoring

--- src/containers/Images/index.js
+++ src/containers/Images/index.js
@@ -6,21 +6,22 @@ import StyledContainer from './style';
 import { getImages } from '../../reducers';
 import debounce from 'lodash.debounce';
 
-const DEBOUNCE_TIME = 800;
+const DEBOUNCE_DELAY = 800;
 
-class Images extends Component {
+export class Images extends Component {
   inputChange = text => {
     if (text) {
       this.props.getImages({ consumer: 'images', query: text.replace(/ /g, '+') });
     }
   };
 
-  debouncedInputChange = debounce(this.inputChange, DEBOUNCE_TIME);
-
   render = () => (
     <div>
       <StyledContainer className="row">
-        <Input onChangeHandler={this.debouncedInputChange} placeholder="search" />
+        <Input
+          onChangeHandler={debounce(this.inputChange, DEBOUNCE_DELAY)}
+          placeholder="search"
+        />
       </StyledContainer>
       <div className="row">
         {this.props.images.map(image => (
