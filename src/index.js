d74eba5eb9f5c07de13065c460c0e2810c76f6f3
Mikalai Sokarau <mikalaisokarau@gmail.com>
2018-07-09 19:01:47 +0300

    quotes refactoring^^

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from "react";
-import { render } from "react-dom";
-import { Provider } from "react-redux";
-import { BrowserRouter, Route } from 'react-router-dom'
-import { createStore, applyMiddleware } from "redux";
-import createSagaMiddleware from "redux-saga";
-import "materialize-css/dist/css/materialize.min.css";
-import "./style.css";
-import App from "./containers/App";
-import { reducer } from "./reducers";
-import { rootSaga } from "./sagas";
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import 'materialize-css/dist/css/materialize.min.css';
+import './style.css';
+import App from './containers/App';
+import { reducer } from './reducers';
+import rootSaga from './sagas';
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(reducer, applyMiddleware(sagaMiddleware));
@@ -17,8 +17,8 @@ sagaMiddleware.run(rootSaga);
 render(
   <Provider store={store}>
     <BrowserRouter>
-      <Route component={App} />  
+      <Route component={App} />
     </BrowserRouter>
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
